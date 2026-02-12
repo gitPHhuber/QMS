b5e99035dc686cb0196bedf25ef4d1a303b106f4
gitPHhuber <gptusers.user1@gmail.com>
2026-02-12 13:49:44 +0300

    fix(rbac): enforce ability checks on new qms routers

--- QMS-Server-master/modules/qms-validation/routes/validationRouter.js
+++ QMS-Server-master/modules/qms-validation/routes/validationRouter.js
@@ -3,14 +3,15 @@ const router = new Router();
 const validationController = require("../controllers/validationController");
 const authMiddleware = require("../../core/middleware/authMiddleware");
 const syncUserMiddleware = require("../../core/middleware/syncUserMiddleware");
+const checkAbility = require("../../core/middleware/checkAbilityMiddleware");
 
 const protect = [authMiddleware, syncUserMiddleware];
 
-router.get("/stats", ...protect, validationController.getStats);
+router.get("/stats", ...protect, checkAbility("validation.read"), validationController.getStats);
 
-router.get("/", ...protect, validationController.getAll);
-router.get("/:id", ...protect, validationController.getOne);
-router.post("/", ...protect, validationController.create);
-router.put("/:id", ...protect, validationController.update);
+router.get("/", ...protect, checkAbility("validation.read"), validationController.getAll);
+router.get("/:id", ...protect, checkAbility("validation.read"), validationController.getOne);
+router.post("/", ...protect, checkAbility("validation.manage"), validationController.create);
+router.put("/:id", ...protect, checkAbility("validation.manage"), validationController.update);
 
 module.exports = router;
